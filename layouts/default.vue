5501a87ea22fa81b5baf54ce971356f7343ad0f4
Gabriel Veres <85896479+GabrielVeres95@users.noreply.github.com>
2023-03-27 17:30:05 +0300

    Testing Login

---
+++ layouts/default.vue
@@ -0,0 +1,23 @@
+<template>
+	<div>
+		<NuxtPage></NuxtPage>
+	</div>
+</template>
+
+<script>
+export default {
+	head() {
+		return {
+			title: 'StaticShield + Nuxtjs',
+			script: [
+				{
+					hid: 'staticshield',
+					src: 'https://staticshield.vercel.app/script.js',
+					'data-site-id': '<DATA-SITE-ID>',
+					'data-cap': '<CAPTION>',
+				},
+			],
+		}
+	},
+}
+</script>
