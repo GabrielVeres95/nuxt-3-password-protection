396a8106b6010df80ba3ee55a7c5252929ca26f0
Gabriel Veres <85896479+GabrielVeres95@users.noreply.github.com>
2023-03-27 17:35:26 +0300

    ss

--- layouts/default.vue
+++ layouts/default.vue
@@ -13,8 +13,8 @@ export default {
 				{
 					hid: 'staticshield',
 					src: 'https://staticshield.vercel.app/script.js',
-					'data-site-id': '<DATA-SITE-ID>',
-					'data-cap': '<CAPTION>',
+					'data-site-id': 'fbe1e7da-d79b-4265-8209-a14f099d838e',
+					'data-cap': 'The password is 123test123',
 				},
 			],
 		}
