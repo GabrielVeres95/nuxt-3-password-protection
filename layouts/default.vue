fe1b3a52dab1dd961735bebf7846593a84249a56
Gabriel Veres <85896479+GabrielVeres95@users.noreply.github.com>
2023-03-27 17:55:15 +0300

    sss

--- layouts/default.vue
+++ layouts/default.vue
@@ -3,21 +3,3 @@
 		<NuxtPage></NuxtPage>
 	</div>
 </template>
-
-<script>
-export default {
-	head() {
-		return {
-			title: 'StaticShield + Nuxtjs',
-			script: [
-				{
-					hid: 'staticshield',
-					src: 'https://staticshield.vercel.app/script.js',
-					'data-site-id': 'c539b96b-1c50-4ae5-9c84-eb411ad8421d',
-					'data-cap': 'TEstingeee',
-				},
-			],
-		}
-	},
-}
-</script>
--- nuxt.config.js
+++ nuxt.config.js
@@ -1,4 +1,16 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
 	css: ['~/assets/styles/main.sass'],
+	app: {
+		head: {
+			script: [
+				{
+					hid: 'staticshield',
+					src: 'https://staticshield.vercel.app/script.js',
+					'data-site-id': 'c539b96b-1c50-4ae5-9c84-eb411ad8421d',
+					'data-cap': 'TEstingeee',
+				},
+			],
+		},
+	},
 })
